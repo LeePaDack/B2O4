0e6ed404ada1bb68787987995dd401690e3f8c11
kimsanghoTT <165999006+kimsanghoTT@users.noreply.github.com>
2024-07-26 15:15:19 +0900

    live 1

---
+++ b2o4-frontend/src/component/WebCam.js
@@ -0,0 +1,34 @@
+import React, { useRef, useEffect } from 'react';
+
+const Webcam = () => {
+    const videoRef = useRef(null);
+
+    const getCamera = () => {
+        
+        navigator.mediaDevices.getUserMedia({
+            video:true
+        })
+        .then((stream) => {
+            let video = videoRef.current;
+            video.srcObject = stream;
+            video.onloadedmetadata = () => {
+                video.play();
+            };
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    useEffect(() => {
+        getCamera();
+    }, [videoRef]);
+
+    return(
+        <div className='webcam-layout-container'>
+            <video className='webcam-container' ref={videoRef}/>
+        </div>
+    )
+};
+
+export default Webcam;
