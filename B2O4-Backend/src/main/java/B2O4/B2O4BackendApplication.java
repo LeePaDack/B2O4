9ea41a510f35e9130170cddb08d1cbb4627e8f87
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-07-26 14:22:20 +0900

    B2O4

---
+++ B2O4-Backend/src/main/java/B2O4/B2O4BackendApplication.java
@@ -0,0 +1,13 @@
+package B2O4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2O4BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2O4BackendApplication.class, args);
+	}
+
+}
---
+++ B2O4-Backend/src/test/java/B2O4/B2O4BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package B2O4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2O4BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
