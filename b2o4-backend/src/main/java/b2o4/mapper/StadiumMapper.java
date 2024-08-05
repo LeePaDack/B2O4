1f925d7a10809b467ca056007f569fd51511d83e
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-08-05 16:29:43 +0900

    경기장 목록 띄우기

---
+++ b2o4-backend/src/main/java/b2o4/mapper/StadiumMapper.java
@@ -0,0 +1,12 @@
+package b2o4.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import b2o4.dto.Stadium;
+
+@Mapper
+public interface StadiumMapper {
+	List<Stadium> stadiumMainList();
+}
