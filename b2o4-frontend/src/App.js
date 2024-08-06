5a2890e3de1da460d6493de670c7cbaa80964d19
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-08-06 17:21:47 +0900

    구장 예약 front

--- b2o4-frontend/src/App.js
+++ b2o4-frontend/src/App.js
@@ -6,6 +6,8 @@ import {BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import BoardContent from './component/boardContent.js';
 import StadiumList from './component/stadiumList.js';
 import BoardUpdate from './component/boardUpdate.js';
+import StadiumDetail from './component/stadiumDetail.js';
+import ReservationStadium from './component/reservationStadium.js';
 
 function App() {
 
@@ -13,21 +15,21 @@ function App() {
   return (
     <div>
       <h1>메인</h1>
+      
       <Router>
         <Link to='/boardMain'>고객센터</Link>
-        <br></br>
-        <Link to='/reservationStadium'>예약관련</Link>
         <br></br>
         <Link to='/StadiumList'>구장정보</Link>
         <br></br>
         <Link to='/'><button>돌아가기</button></Link>
         <Routes>
           <Route path='stadiumList' element={<StadiumList/>}/>
-          <Route path='reservationStadium' element={<Date2/>}/>
+          <Route path='/stadiumDetail/:stadiumNo' element={<StadiumDetail/>}/>
           <Route path='/boardPosting' element={<BoardPosting/>}/>
           <Route path='/boardUpdate' element={<BoardUpdate/>}/>
           <Route path='/boardMain' element={<BoardMain/>}/>
           <Route path='/boardContent/:boardNo' element={<BoardContent/>}/>
+          <Route path='/reservationStadium/:stadiumNo' element={<ReservationStadium/>}/>
         </Routes>
       </Router>
     </div>
---
+++ b2o4-frontend/src/component/reservationStadium.js
@@ -0,0 +1,95 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+const ReservationStadium = () => {
+
+  const [personCount, setPersonCount] = useState(1); 
+
+  const [minDate, setMinDate] = useState("");
+  const [maxDate, setMaxDate] = useState("");
+
+  const location = useLocation();
+  const [stadiums, setStadiums] = useState([]);
+  const stadium = location.state.stadium;
+  
+  console.log("location",location);
+
+  useEffect(() => {
+      StadiumReservationList();
+  }, []);
+
+  const StadiumReservationList = async() => {
+      const res = await axios.get('/stadiums');
+      setStadiums(res.data);
+  };
+
+  useEffect(() => {
+    const getKSTDateString = (date) => {
+      const offset = 9 * 60;
+      const kstDate = new Date(date.getTime() + offset * 60 * 1000);
+      return kstDate.toISOString().split("T")[0];
+    };
+
+    const today = new Date();
+    const min = getKSTDateString(today);
+
+    const twoWeeksLater = new Date(today.getTime() + 14 * 24 * 60 * 60 * 1000);
+    const max = getKSTDateString(twoWeeksLater);
+
+    setMinDate(min);
+    setMaxDate(max);
+  }, []);
+
+
+  const handlePersonCountChange = (event) => {
+    setPersonCount(parseInt(event.target.value));
+  };
+
+  const calculateTotalPrice = () => {
+    return stadium.stadiumPrice * personCount;
+  };
+  return (
+    <div>
+      <h6>예매하기</h6>
+      <input
+        type="date"
+        className="reservationDate"
+        name="closingDate"
+        min={minDate}
+        max={maxDate}
+      />
+      &nbsp; &nbsp; &nbsp;
+      <select name="reservationTime">
+        <option>예약 시간</option>
+        <option>06:00 ~ 08:00</option>
+        <option>08:00 ~ 10:00</option>
+        <option>10:00 ~ 12:00</option>
+        <option>12:00 ~ 14:00</option>
+        <option>14:00 ~ 16:00</option>
+        <option>16:00 ~ 18:00</option>
+        <option>18:00 ~ 20:00</option>
+        <option>20:00 ~ 22:00</option>
+        <option>22:00 ~ 00:00</option>
+        <option>00:00 ~ 02:00</option>
+      </select>
+      &nbsp; &nbsp; &nbsp;
+      <select value={personCount} onChange={handlePersonCountChange}> 
+
+        <option>인원수{/*{구장 수용인원 DB 에서 가져오기}*/}</option>
+        <option value="1">1명</option>
+        <option value="2">2명</option>
+        <option value="3">3명</option>
+        <option>...</option>
+      </select>
+      <tr>
+        <td>구장이름<span id="stadiumName">{stadium.stadiumName}</span></td>
+        <td>총액 : <span id="totalPrice">{calculateTotalPrice().toLocaleString()}</span></td>
+        </tr>
+        <button type="button">결제하기</button> {/* toss 로 넘어가는 루트 만들어야함 */}
+    </div>
+    
+  );
+};
+
+export default ReservationStadium;
