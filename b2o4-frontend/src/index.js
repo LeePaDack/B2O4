29c2ece083c4dd4950a0babc165b39d22611f7a5
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-08-07 17:09:11 +0900

    수정

--- b2o4-frontend/src/index.js
+++ b2o4-frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,4 +17,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-// *********************************** BrowserRouter 추가!!!!!!!!!!!!!!!!!!!!!!!!!!
