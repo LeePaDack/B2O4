2a83a4f00c6a41849f915f6cd096201d32b85402
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-09-03 15:25:23 +0900

    webconfig 수정

--- b2o4-backend/src/main/java/b2o4/common/config/WebConfig.java
+++ b2o4-backend/src/main/java/b2o4/common/config/WebConfig.java
@@ -17,14 +17,14 @@ public class WebConfig implements WebMvcConfigurer{
 	}
 	
 
-
+	/*
 	// 이미지 저장 경로 접근 허용
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("C:/Users/user1/Desktop/B2O4/b2o4-frontend/public/images");
     }
-
+    */
 
 
 }
