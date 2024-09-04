a7d8606f728a0c917239867b87638a8ef0751fa7
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-09-04 11:35:50 +0900

    주소값 수정

--- b2o4-frontend/src/components/Login/FindPw.js
+++ b2o4-frontend/src/components/Login/FindPw.js
@@ -213,7 +213,7 @@ const FindPw = () => {
                 </Link>
               </li>
               <li className="login-item">
-                <Link to="/register" className="login-link">
+                <Link to="/Signup" className="login-link">
                   <img src="soccer.jpg" />
                   &nbsp;회원가입
                 </Link>
